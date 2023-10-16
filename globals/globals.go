f9a2141224bb9fbd3b9bb8e51260af38d38a1b44
SuperMarioDaBom <ajguerra2001@gmail.com>
2023-10-15 23:41:05 -0700

    Add grpc to globals

--- globals/globals.go
+++ globals/globals.go
@@ -5,6 +5,10 @@ import (
 	"github.com/PretendoNetwork/plogger-go"
 	"github.com/PretendoNetwork/super-mario-maker-secure/database"
 	"github.com/minio/minio-go/v7"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+
+	pb "github.com/PretendoNetwork/grpc-go/account"
 )
 
 var Logger = plogger.NewLogger()
@@ -12,3 +16,6 @@ var NEXServer *nex.Server
 var MinIOClient *minio.Client
 var Presigner *S3Presigner
 var DataStoreIDGenerators []*DataStoreIDGenerator
+var GRPCAccountClientConnection *grpc.ClientConn
+var GRPCAccountClient pb.AccountClient
+var GRPCAccountCommonMetadata metadata.MD
